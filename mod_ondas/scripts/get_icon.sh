d676156c95100b4c4a2fed88e5a743f21362e708
CHM13 <flavia@smm.mil.br>
2019-07-31 18:39:03 +0000

    Script para pegar o vento do ICON na DPNS24, testado e correto

---
+++ mod_ondas/scripts/get_icon.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -x
+##################################################
+# Script que pegar o vento a 10 m do ICON na     #
+# DPNS24 para a rodada WW3                       #
+#                                                #
+# JUL2019                                        #  
+# Autoras: 1T(RM2-T) Andressa D'Agostini         #
+#          Bruna Reis                            #
+##################################################
+
+# ---------------------
+# Definição diretórios
+
+source ~/mod_ondas/fixos/dir.sh
+
+if [ $# -lt 1 ]
+   then
+   echo "+------------------Utilização----------------+"
+   echo "  Script para pegar o dado do ICON na DPNS24  "
+   echo "               27km, 6-6 horas                "
+   echo "                                              "
+   echo "          ./get_icon.sh hh yyyymmdd           "
+   echo "                                              "
+   echo "       ex: ./get_icon.sh 00 20190716          "
+   echo "+--------------------------------------------+"
+   exit
+fi
+
+HSIM=$1  
+HSTART=0
+HSTOP=120
+
+if [ $# -eq 1 ]; then
+   AMD=`cat ~/datas/datacorrente${HSIM}`
+elif [ $# -eq 2 ]; then
+   AMD=$2
+fi
+
+# ---------------------
+# Definição diretórios
+DIRICON=${WW3DIR}/input/vento/icon
+DIRICONdados=${DIRICON}/dados
+WORKDIR=${DIRICONdados}/work
+ICONDATA=/mnt/nfs/dpns24/icondata/wind${HSIM}
+ICONARQz=icon_reg_to_client_bsm1_${AMD}${HSIM}.grib2.bz2
+ICONARQ=icon_reg_to_client_bsm1_${AMD}${HSIM}.grib2
+
+# Copiando arquivo para o diretório do work
+cp ${ICONDATA}/${ICONARQz} ${WORKDIR}
+# Descompactando o arquivo no work
+${p_bunzip2} -f ${WORKDIR}/${ICONARQz}
+# Transformando o arquivo em netcdf
+${p_wgrib2} ${WORKDIR}/${ICONARQ} -netcdf ${WORKDIR}/icon.${AMD}${HSIM}.nc
+mv ${WORKDIR}/icon.${AMD}${HSIM}.nc ${DIRICON}
+
+# Limpando o diretorio work
+for file in "${WORKDIR}"/*
+do
+  rm "$file"
+done
