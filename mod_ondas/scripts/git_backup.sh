05e1aaf13f46cb503e5865dcba7a1ec9c257d529
CHM13 <flavia@smm.mil.br>
2019-08-02 20:10:53 +0000

    Atualização

---
+++ mod_ondas/scripts/git_backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+##################################################
+# Script para realizar o backup dos scritps com  #
+# as modificações no github                      #
+#                                                #
+# AGO2019                                        #  
+# 1T(RM2-T) Andressa D'Agostini                  #
+##################################################
+
+# -----------------------------------------
+# Definição diretórios raiz e do wavewatch
+source ~/mod_ondas/fixos/dir.sh
+source ~/.bashrc
+
+SCRIPTDIR=${WW3DIR}/scripts
+
+for script in ${SCRIPTDIR}/*; do
+  git add ${script}
+  echo ''
+  echo ' Enviando script '${script}' para o GitHub '
+  echo ''
+  git commit -m " Atualização "
+  git push origin master
+done
+
+FIXODIR=${WW3DIR}/fixos
+
+for inp in ${FIXODIR}/*; do
+  git add ${inp}
+  echo ''
+  echo ' Enviando arquivo inp '${inp}' para o GitHub '
+  echo ''
+  git commit -m " Atualização "
+  git push origin master
+done
+
