42d3368390b47d55a1156a61354e261963187673
CHM13 <flavia@smm.mil.br>
2019-08-08 20:59:12 +0000

    Melhorias e Correção de erros encontrados

--- mod_ondas/scripts/ww3Operacional.sh
+++ mod_ondas/scripts/ww3Operacional.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+####/bin/bash
 ##################################################
 # Script que realiza o processamento do WW3      #
 #                                                #
@@ -11,6 +12,7 @@
 # Definição diretórios raiz e do wavewatch
 source ~/mod_ondas/fixos/dir.sh
 source ~/wavewatch/setww3v607.sh
+source ~/.bashrc
 
 if [ $# -lt 1 ]
    then
@@ -29,13 +31,15 @@ HSIM=$1
 # -------------------------------
 #  Definindo informação de datas
 
-if [ $# -eq 2 ]; then
+if [ $# -eq 1 ]; then
    AMD=`cat ~/datas/datacorrente${HSIM}`
-elif [ $# -eq 3 ]; then
-   AMD=$3
+elif [ $# -eq 2 ]; then
+   AMD=$2
 fi
 
 AMDm1=`cat ~/datas/datacorrente_m100`
+
+echo ' Rodada '${AMD}' '${HSIM}
 
 # -----------------------
 #  Definindo diretorios
@@ -47,6 +51,7 @@ LOGDIR=${WW3DIR}/logs
 FLAGDIR=${WW3DIR}/flags
 OUTDIR=${WW3DIR}/output
 GELODIR=${DIRICE}
+BCKDIR=${WW3DIR}/backup
 
 # ------------------------
 #  Definindo as forçantes
@@ -54,10 +59,9 @@ GELODIR=${DIRICE}
 forc1=gfs
 forc2=gfs12
 forc3=icon
-#forc4=icon13
+forc4=icon13
 forc5=cosmo
-#FORCs=(${forc1} ${forc2} ${forc3}  ${forc4} ${forc5})
-FORCs=(${forc1} ${forc2} ${forc3} ${forc5})
+FORCs=(${forc1}  ${forc2} ${forc3}  ${forc4})
 
 # Flags de tempo para o while
 Abort=480  # minutos - 8 horas de limite na tentativa de rodada do WW3 
@@ -65,11 +69,19 @@ Tspended=0
 
 OUTDIRop='/data2/operador/mod_ondas/ww3_418/output'
 
-#if [ -e ${OUTDIRop}/ww3${forc1}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ -e ${OUTDIRop}/ww3${forc3}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ -e ${OUTDIRop}/ww3${forc5}/wave.${AMD}/WW3MET_${HSIM}_SAFO ]; then
+if [ -e ${OUTDIRop}/ww3${forc1}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ -e ${OUTDIRop}/ww3${forc3}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ -e ${OUTDIRop}/ww3${forc5}/wave.${AMD}/WW3MET_${HSIM}_SAFO ]; then
+
+  echo ' '
+  echo ' Rodada do WW3 v4.18 '${AMD}' '${HSIM}' terminou de rodar. '
+  echo ' Iniciando a rodada do WW3 v6.07 '${AMD}' '${HSIM}
+  echo ' '
 
   while [ ${Abort} -gt ${Tspended} ]; do
+
     for FORC in "${FORCs[@]}"; do
+
       if [ ${FORC} = "gfs" ] || [ ${FORC} = "gfs12" ] || [ ${FORC} = "icon" ]; then #|| [ ${FORC} = "icon13" ]; then 
+
         if [ ! -e ${GELODIR}/seaice.${AMDm1}.nc ]; then
           echo ' '
           echo ' Pegando e interpolando a máscara de gelo '
@@ -77,64 +89,82 @@ OUTDIRop='/data2/operador/mod_ondas/ww3_418/output'
           ${WW3DIR}/scripts/get_ice.sh
           ${WW3DIR}/scripts/prnc_wnd_ice.sh ice 00
         fi
+
         if [ ! -e ${DIRWND}/${FORC}/${FORC}.${AMD}${HSIM}.nc ]; then
+
           echo ' '
           echo ' Pegando o vento '${FORC}' Data e HH: '${AMD}${HSIM}
           echo ' '
           ${WW3DIR}/scripts/get_${FORC}.sh ${HSIM}
+
         elif [ -e ${DIRWND}/${FORC}/${FORC}.${AMD}${HSIM}.nc ] && [ ! -e ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO ]; then
+
           echo ' '
           echo ' Iniciando a rodada do WW3'${FORC}' Data e HH: '${AMD}${HSIM}
           echo ' '
           ${WW3DIR}/scripts/prnc_wnd_ice.sh ${FORC} ${HSIM}
           ${WW3DIR}/scripts/ww3Exec_wnd.sh ${FORC} ${HSIM}
           ${WW3DIR}/scripts/pos_proc.sh ${FORC} ${HSIM}
-          if [ -e ${OURDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.met ] && [ -e ${OUTDIR}/met.t${HSIM}z.ctl ]; then
+
+#          if [ -e ${OUTDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.met5_${FORC} ] && [ -e ${OUTDIR}/ww3${FORC}/${AMD}/met.t${HSIM}z.ctl ]; then
+          if [ -e ${OUTDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.met5_${FORC} ] && [ -e ${BCKDIR}/ww3${FORC}/ww3${FORC}_met_${AMD}${HSIM}.nc ]; then
             touch ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO
           fi
+
         fi 
+
       elif [ ${FORC} = "cosmo" ]; then
+
         if [ ! -e ${DIRWND}/${FORC}/${FORC}.${AMD}${HSIM}.nc ]; then
+
           echo ' '
           echo ' Pegando o vento '${FORC}' Data e HH: '${AMD}${HSIM}
           echo ' '
           ${WW3DIR}/scripts/get_${FORC}.sh ${HSIM}
+
         elif [ -e ${OUTDIR}/ww3icon/${AMD}/nest.t${HSIM}z.met5_icon ] && [ -e ${DIRWND}/${FORC}/${FORC}.${AMD}${HSIM}.nc ] && [ ! -e ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO ] || [ -e ${OUTDIR}/ww3icon13/${AMD}/nest.t${HSIM}z.met5_icon13 ] && [ -e ${DIRWND}/${FORC}/${FORC}.${AMD}${HSIM}.nc ] && [ ! -e ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO ]; then
+
           echo ' '
           echo ' Iniciando a rodada do WW3'${FORC}' Data e HH: '${AMD}${HSIM}
           echo ' '
           ${WW3DIR}/scripts/prnc_wnd_ice.sh ${FORC} ${HSIM}
           ${WW3DIR}/scripts/ww3Exec_wnd.sh ${FORC} ${HSIM}
           ${WW3DIR}/scripts/pos_proc.sh ${FORC} ${HSIM}      
-          if [ -e ${OURDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.met ] && [ -e ${OUTDIR}/met.t${HSIM}z.ctl ]; then
+
+#          if [ -e ${OUTDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.met5_${FORC} ] && [ -e ${OUTDIR}/ww3${FORC}/${AMD}/met.t${HSIM}z.ctl ]; then
+          if [ -e ${OUTDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.met5_${FORC} ] && [ -e ${BCKDIR}/ww3${FORC}/ww3${FORC}_met_${AMD}${HSIM}.nc ]; then
             touch ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO
           fi
+
         fi
-#    elif [ -e ${FLAGDIR}/WW3${forc1}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc2}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc3}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc4}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc5}_${AMD}${HSIM}_SAFO ]; then
-      elif [ -f ${FLAGDIR}/WW3${forc1}_${AMD}${HSIM}_SAFO ] && [ -f ${FLAGDIR}/WW3${forc2}_${AMD}${HSIM}_SAFO ] && [ -f ${FLAGDIR}/WW3${forc3}_${AMD}${HSIM}_SAFO ] && [ -f ${FLAGDIR}/WW3${forc5}_${AMD}${HSIM}_SAFO ]; then
+    elif [ -e ${FLAGDIR}/WW3${forc1}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc2}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc3}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc4}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc5}_${AMD}${HSIM}_SAFO ]; then
         echo ' '
         echo ' Todos WW3 terminaram de rodar! '
         echo ' '
         exit 1
+
       else
         continue
       fi 
+
     done
+
     echo ' '
     echo ' Aguardando Forçante de Vento chegar... sleep: '${Tspended}
     echo ' '
     sleep 60
     Aux=`expr ${Tspended} + 1`
     Tspended=${Aux}
+
   done
 
-#elif [ ! -e ${OUTDIRop}/ww3${forc1}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ ! -e ${OUTDIRop}/ww3${forc3}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ ! -e ${OUTDIRop}/ww3${forc5}/wave.${AMD}/WW3MET_${HSIM}_SAFO ]; then
+elif [ ! -e ${OUTDIRop}/ww3${forc1}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ ! -e ${OUTDIRop}/ww3${forc3}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ ! -e ${OUTDIRop}/ww3${forc5}/wave.${AMD}/WW3MET_${HSIM}_SAFO ]; then
 
-#  echo ' '
-#  echo ' Aguardando ww3 versão 4.18 terminar de rodar... sleep: '${Tspended}
-#  echo ' '
-#  sleep 60
-#  Aux=`expr ${Tspended} + 1`
-#  Tspended=${Aux}
+  echo ' '
+  echo ' Aguardando ww3 versão 4.18 terminar de rodar... sleep: '${Tspended}
+  echo ' '
+  sleep 60
+  Aux=`expr ${Tspended} + 1`
+  Tspended=${Aux}
 
-#fi
+fi
