70d17fa552abf7189ec47825a27c60fd0b247071
CHM13 <flavia@smm.mil.br>
2019-09-19 18:01:22 +0000

    Atualização

--- mod_ondas/scripts/prnc_wnd_ice.sh
+++ mod_ondas/scripts/prnc_wnd_ice.sh
@@ -21,7 +21,7 @@ if [ $# -lt 2 ]
    echo "                                              "
    echo " ./prnc_wnd_ice.sh (Vwnd ou ice) hh yyyymmdd  "
    echo "                                              "
-   echo "   Vwnd = (gfs, icon, cosmo, gfs12, icon13)   "
+   echo "   Vwnd = (gfs, icon, cosmo, gfs12, ico13)    "
    echo "                                              "
    echo "    ex: ./prnc_wnd_ice.sh gfs 00 20190716     "
    echo "+--------------------------------------------+"
@@ -44,7 +44,6 @@ fi
 # -----------------------
 #  Definindo diretorios
 
-WORKDIR=${WW3DIR}/work
 GRDDIR=${WW3DIR}/grids
 FIXODIR=${WW3DIR}/fixos
 
@@ -52,34 +51,37 @@ FIXODIR=${WW3DIR}/fixos
 #  Realização do prnc
 
 if [ ${FORC} = "ice" ]; then 
-   FORCDIR=${WW3DIR}/input/gelo
-   ln -sf ${GRDDIR}/mod_def.${FORC} ${WORKDIR}/mod_def.ww3
-   ln -sf ${FORCDIR}/seaice.${AMD}.nc ${WORKDIR}/ice.nc
-   ln -sf ${FIXODIR}/ww3_prnc.inp.${FORC} ${WORKDIR}/ww3_prnc.inp
-   echo " "
-   echo " Executando o prnc "${FORC} ${AMD}
-   echo " "
-   cd ${WORKDIR}
-   ww3_prnc
-   mv ${WORKDIR}/ice.ww3 ${FORCDIR}/${FORC}.${AMD}.${FORC}
+  FORCDIR=${WW3DIR}/input/gelo
+  WORKDIR=${DIRICE}/dados/work
+  ln -sf ${GRDDIR}/mod_def.${FORC} ${WORKDIR}/mod_def.ww3
+  ln -sf ${FORCDIR}/seaice.${AMD}.nc ${WORKDIR}/ice.nc
+  ln -sf ${FIXODIR}/ww3_prnc.inp.${FORC} ${WORKDIR}/ww3_prnc.inp
+  echo " "
+  echo " Executando o prnc "${FORC} ${AMD}
+  echo " "
+  cd ${WORKDIR}
+  ww3_prnc
+  mv ${WORKDIR}/ice.ww3 ${FORCDIR}/${FORC}.${AMD}.${FORC}
+  for filename in ${WORKDIR}/*; do
+     rm $filename
+  done
 else
-   FORCDIR=${WW3DIR}/input/vento/${FORC}
-   if [ ${FORC} = "cosmo" ]; then
-     ln -sf ${GRDDIR}/mod_def.met5_${FORC} ${WORKDIR}/mod_def.ww3
-   else
-     ln -sf ${GRDDIR}/mod_def.${FORC} ${WORKDIR}/mod_def.ww3
-   fi
-   ln -sf ${FORCDIR}/${FORC}.${AMD}${HSIM}.nc ${WORKDIR}/wnd.nc
-   ln -sf ${FIXODIR}/ww3_prnc.inp.${FORC} ${WORKDIR}/ww3_prnc.inp
-   echo " "
-   echo " Executando o prnc "${FORC} ${AMD}${HSIM}
-   echo " "
-   cd ${WORKDIR}
-   ww3_prnc
-   cp ${WORKDIR}/wind.ww3 ${FORCDIR}/wind.${AMD}${HSIM}.${FORC}
+  FORCDIR=${WW3DIR}/input/vento/${FORC}
+  WORKDIR=${FORCDIR}/dados/work
+  if [ ${FORC} = "cosmo" ]; then
+    ln -sf ${GRDDIR}/mod_def.met5_${FORC} ${WORKDIR}/mod_def.ww3
+  else
+    ln -sf ${GRDDIR}/mod_def.${FORC} ${WORKDIR}/mod_def.ww3
+  fi
+ ln -sf ${FORCDIR}/${FORC}.${AMD}${HSIM}.nc ${WORKDIR}/wnd.nc
+  ln -sf ${FIXODIR}/ww3_prnc.inp.${FORC} ${WORKDIR}/ww3_prnc.inp
+  echo " "
+  echo " Executando o prnc "${FORC} ${AMD}${HSIM}
+  echo " "
+  cd ${WORKDIR}
+  ww3_prnc
+  cp ${WORKDIR}/wind.ww3 ${FORCDIR}/wind.${AMD}${HSIM}.${FORC}
+  for filename in ${WORKDIR}/*; do
+     rm $filename
+  done
 fi
-
-
-for filename in ${WORKDIR}/*; do
- rm $filename
-done
