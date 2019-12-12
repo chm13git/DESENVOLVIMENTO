4be74ea19205000c8428a391118fc97f15811e0e
CHM13 <flavia@smm.mil.br>
2019-12-12 16:56:51 +0000

    Atualização

--- mod_ondas/scripts/apaga_dados_antigos.sh
+++ mod_ondas/scripts/apaga_dados_antigos.sh
@@ -22,19 +22,28 @@ for arq in `find ${ICEDIR} -mtime +5 -name "ice.*" `; do
   rm $arq
 done
 
-forc1=gfs
-forc2=gfs12
-forc3=icon
-forc4=ico13
-forc5=cosmo
+forc1=gfs12
+forc2=ico13
+forc3=cosmo
+forc4=icon
+forc5=gfs
 FORCs=(${forc1}  ${forc2} ${forc3}  ${forc4} ${forc5})
 
 for FORC in "${FORCs[@]}"; do
   WNDDIR=${VENTODIR}/${FORC}
-  BCKDIR=${WW3DIR}/backup/ww3${FORC}
+  SPECDIR=${WW3DIR}/backup/ww3${FORC}/specs
+  TABSDIR=${WW3DIR}/backup/ww3${FORC}/tabs
   OUTDIR=${WW3DIR}/output/ww3${FORC}
   RESTDIR=${WW3DIR}/restart/ww3${FORC}
   for arq in `find ${WNDDIR} -mtime +3 -name "wind.*" `; do
+    echo $arq
+    rm $arq
+  done
+  for arq in `find ${TABSDIR} -mtime +3 -name "tab_*" `; do
+    echo $arq
+    rm $arq
+  done
+  for arq in `find ${SPECDIR} -mtime +3 -name "spec_*" `; do
     echo $arq
     rm $arq
   done
@@ -42,7 +51,7 @@ for FORC in "${FORCs[@]}"; do
     echo $arq
     rm $arq
   done
-  for arq in `find ${RESTDIR} -mtime +2 -name "restart.*" `; do
+  for arq in `find ${RESTDIR} -mtime +5 -name "restart.*" `; do
     echo $arq
     rm $arq
   done
