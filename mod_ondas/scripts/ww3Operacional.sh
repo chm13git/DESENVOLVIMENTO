97cb3304ba277584f129ba247533581ac366015b
CHM13 <flavia@smm.mil.br>
2019-12-12 19:23:09 +0000

    Atualização

--- mod_ondas/scripts/ww3Operacional.sh
+++ mod_ondas/scripts/ww3Operacional.sh
@@ -55,13 +55,11 @@ BCKDIR=${WW3DIR}/backup
 # ------------------------
 #  Definindo as forçantes
 
-forc1=gfs
-forc2=gfs12
-forc3=icon
-forc4=cosmo
-forc5=ico13
-FORCs=(${forc1}  ${forc2} ${forc3}  ${forc4} ${forc5})
-#FORCs=(${forc1}  ${forc2} ${forc3}  ${forc4})
+forc1=gfs12
+forc2=ico13
+forc3=cosmo
+
+FORCs=(${forc1} ${forc2} ${forc3})
 
 # Flags de tempo para o while
 Abort=480  # minutos - 8 horas de limite na tentativa de rodada do WW3 
@@ -72,7 +70,7 @@ while [ ${Abort} -gt ${Tspended} ]; do
   for FORC in "${FORCs[@]}"; do
     
     if [ ${FORC} = "cosmo" ]; then
-      if [ -e ${OUTDIR}/ww3ico13/${AMD}/nest.t${HSIM}z.met5_ico13 ] && [ -e ${DIRWND}/${FORC}/wind.${AMD}${HSIM}.${FORC} ] && [ ! -e ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO ] || [ -e ${OUTDIR}/ww3icon/${AMD}/nest.t${HSIM}z.met5_icon ] && [ -e ${DIRWND}/${FORC}/wind.${AMD}${HSIM}.${FORC} ] && [ ! -e ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO ]; then
+      if [ -e ${OUTDIR}/ww3ico13/${AMD}/nest.t${HSIM}z.met5_ico13 ] && [ -e ${DIRWND}/${FORC}/wind.${AMD}${HSIM}.${FORC} ] && [ ! -e ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO ]; then
         echo ' '
         echo ' Iniciando a rodada do WW3'${FORC}' Data e HH: '${AMD}${HSIM}
         echo ' '
@@ -89,15 +87,14 @@ while [ ${Abort} -gt ${Tspended} ]; do
         echo ' '
         ${WW3DIR}/scripts/ww3Exec_wnd.sh ${FORC} ${HSIM} ${AMD}
         ${WW3DIR}/scripts/pos_proc.sh ${FORC} ${HSIM} ${AMD}
-        if [ -e ${OUTDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.met5_${FORC} ] && [ -e ${BCKDIR}/ww3${FORC}/ww3${FORC}_met_${AMD}${HSIM}.nc ]; then
+        if [ -e ${OUTDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.met5_${FORC} ] && [ -e ${OUTDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.glo_${FORC} ] && [ -e ${OUTDIR}/ww3${FORC}/${AMD}/out_grd.t${HSIM}z.ant5_${FORC} ] && [ -e ${BCKDIR}/ww3${FORC}/ww3${FORC}_met_${AMD}${HSIM}.nc ]; then
           touch ${FLAGDIR}/WW3${FORC}_${AMD}${HSIM}_SAFO
         fi
       fi
     fi
   done
 
-  if [ -e ${FLAGDIR}/WW3${forc1}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc2}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc3}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc4}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc5}_${AMD}${HSIM}_SAFO ]; then
-#  if [ -e ${FLAGDIR}/WW3${forc1}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc2}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc3}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc4}_${AMD}${HSIM}_SAFO ]; then
+  if [ -e ${FLAGDIR}/WW3${forc1}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc2}_${AMD}${HSIM}_SAFO ] && [ -e ${FLAGDIR}/WW3${forc3}_${AMD}${HSIM}_SAFO ]; then
     echo ' '
     echo ' Todos WW3 terminaram de rodar! '
     echo ' '
@@ -112,19 +109,3 @@ while [ ${Abort} -gt ${Tspended} ]; do
   fi 
 
 done
-
-#OUTDIRop='/data2/operador/mod_ondas/ww3_418/output'
-#if [ -e ${OUTDIRop}/ww3${forc1}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ -e ${OUTDIRop}/ww3${forc3}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ -e ${OUTDIRop}/ww3${forc5}/wave.${AMD}/WW3MET_${HSIM}_SAFO ]; then
-#  echo ' '
-#  echo ' Rodada do WW3 v4.18 '${AMD}' '${HSIM}' terminou de rodar. '
-#  echo ' Iniciando a rodada do WW3 v6.07 '${AMD}' '${HSIM}
-#  echo ' '
-#done
-#elif [ ! -e ${OUTDIRop}/ww3${forc1}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ ! -e ${OUTDIRop}/ww3${forc3}/wave.${AMD}/WW3MET_${HSIM}_SAFO ] && [ ! -e ${OUTDIRop}/ww3${forc5}/wave.${AMD}/WW3MET_${HSIM}_SAFO ]; then
-#  echo ' '
-#  echo ' Aguardando ww3 versão 4.18 terminar de rodar... sleep: '${Tspended}
-#  echo ' '
-#  sleep 60
-#  Aux=`expr ${Tspended} + 1`
-#  Tspended=${Aux}
-#fi
