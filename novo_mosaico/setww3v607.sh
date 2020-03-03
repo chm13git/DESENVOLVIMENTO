e0a9dbbfaf8b5f5344392fdcece3d2562a1747df
CHM13 <flavia@smm.mil.br>
2020-03-03 17:57:14 +0000

    teste

---
+++ novo_mosaico/setww3v607.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export NETCDF_CONFIG=/data1/ww3desenv/home/libs/netcdf/bin/nc-config
+export WWATCH3_NETCDF=NC4
+
+PATH=$PATH:/data1/ww3desenv/home/wavewatch/ww3v607/model/bin
+export PATH
+PATH=$PATH:/data1/ww3desenv/home/wavewatch/ww3v607/model/exe
+export PATH
